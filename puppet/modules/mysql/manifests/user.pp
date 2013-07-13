7cf84e56503e10f3cd48afcb2b3976aa6202284f
hironomiu <hironomiu@example.com>
2013-07-13 10:38:18 +0900

    削除

--- puppet/modules/mysql/manifests/user.pp
+++
@@ -1,6 +0,0 @@
-class mysql::user {
-    exec { "create-demo-db":
-        unless => "/usr/bin/mysql -udemouser -pdemopass -e \"show databases;\"",
-        command => "/usr/bin/mysql -uroot -e \"create database groupwork; grant all on groupwork.* to demouser@localhost identified by 'demopass';drop user 'root'@'127.0.0.1','root'@'::1',''@'localhost',''@'treasure2013.local','root'@'treasure2013.local';\"",
-    }
-}
