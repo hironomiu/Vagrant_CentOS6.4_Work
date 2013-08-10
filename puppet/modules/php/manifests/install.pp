cabb72c750bcd0a1895f9eaa3f17d57132167410
hironomiu <hironori_miura@voyagegroup.com>
2013-08-10 10:51:37 +0900

    slimバージョンアップに備えてmcryptを追加

--- puppet/modules/php/manifests/install.pp
+++ puppet/modules/php/manifests/install.pp
@@ -25,4 +25,21 @@ class php::install{
         ensure => installed,
         require => Yumrepo['php-remi'],
     }
+
+    yumrepo { 'php-epel':
+        descr => 'epel repo',
+        mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch',
+        enabled    => 1,
+        gpgcheck   => 1,
+        gpgkey     => 'https://fedoraproject.org/static/0608B895.txt',
+    }
+
+    package{
+        [
+        'php-mcrypt',
+        ]:
+        provider => 'yum',
+        ensure => installed,
+        require => Yumrepo['php-epel'],
+    }
 }
